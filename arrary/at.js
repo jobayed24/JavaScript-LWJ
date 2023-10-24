762a2ccdf06757f5c6a65bee666143dcda5d0a21
jobayed24 <jkhouse117@gmail.com>
2023-10-24 20:09:47 +0600

    I cover here at , concat , and copywithin method in array here

---
+++ arrary/at.js
@@ -0,0 +1,5 @@
+const fruits=['apple','banana','graps','pineapple'];
+
+let getFruitName=fruits.at(-1);
+
+console.dir(getFruitName);
---
+++ arrary/concate.js
@@ -0,0 +1,8 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+
+const myCopyWithIn=fruits.copyWithin(2,1);
+
+console.log(myCopyWithIn);
+
+console.log(fruits)
