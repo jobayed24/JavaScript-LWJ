c7df75f7bd245fb5524073de2e6a006b1d6bef5e
jobayed24 <jkhouse117@gmail.com>
2023-10-25 15:50:36 +0600

    find the heighest value from an array which gives you new array with passed elements

--- arrary/filter.js
+++ arrary/filter.js
@@ -1,11 +1,16 @@
 const ages = [32, 33, 16, 40];
 
-function checkHeigherValue(el){
-    return el>20;
+function checkData1(){
+    function checkData(el){
+        let getValue=document.getElementById('submit').value;
+        return el>getValue;
+    }
+
+    const getHigherValue=ages.filter(checkData);
+    console.log(getHigherValue)
 }
 
-const getHigherValue=ages.filter(checkHeigherValue);
 
-console.log(getHigherValue)
 
-console.log(ages)
+
+ 
