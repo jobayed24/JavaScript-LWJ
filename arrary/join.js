e4a05a796302c5f78e1011d07521fbce54668fa0
jobayed24 <jkhouse117@gmail.com>
2023-10-29 19:14:50 +0600

    length property added the length in an array

---
+++ arrary/join.js
@@ -0,0 +1,5 @@
+const fruits=['jackfruits','pineapple','pineapple','orange','orange'];
+
+const makeFruitsAsString=fruits.join('  |  ');
+
+console.log(makeFruitsAsString)
---
+++ arrary/length.js
@@ -0,0 +1,6 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.length=2;
+
+console.log(fruits);
+
