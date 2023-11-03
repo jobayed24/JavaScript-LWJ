1cfe7ef00e54b6bfaf399f7934f8c5a523015026
jobayed24 <jkhouse117@gmail.com>
2023-11-03 13:58:23 +0600

    parse data from json using parse method in js

--- json/script.js
+++ json/script.js
@@ -1,9 +1,9 @@
-let myObj = { name:"John", age:31, city:"New York" };
+let myObj = '{"name":"John","age":31,"city":"New York"}';
 
-let myOBJSting=JSON.stringify(myObj);
+let myOBJSting=JSON.parse(myObj);
 
 
 
-console.log(myObj);
+// console.log(myObj);
 
 console.log( myOBJSting);
