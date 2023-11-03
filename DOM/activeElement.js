6fdb6fdb8fbe9e13237d92fb17e684866196f141
jobayed24 <jkhouse117@gmail.com>
2023-11-03 16:36:45 +0600

    get activeElement with tagname and print those in a div which name demo id

---
+++ DOM/activeElement.js
@@ -0,0 +1,5 @@
+function getElement(){
+    let getElement=document.activeElement.tagName;
+
+document.getElementById('demo').innerHTML=getElement;
+}
--- json/script.js
+++ json/script.js
@@ -1,12 +1,7 @@
 ///reviver function
 
-let myObj = '{"name":"John","age":31,"city":"New York"}';
+let myObj = {"name":"John","age":31,"city":"New York"};
 
-let getObFromSt=JSON.parse(myObj,function(key,value){
-    if(key=="city"){
-        return value.toUpperCase();
-    }else{
-        return value;
-    }
-})
-console.log(getObFromSt.city)
+let JSSting=JSON.stringify(myObj,null,10);
+
+console.log(JSSting)
