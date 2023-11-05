547227f551a6c747fae131750a08242831e697eb
jobayed24 <jkhouse117@gmail.com>
2023-11-05 16:26:25 +0600

    getForm using namedItem form and Forms method

--- DOM/activeElement.js
+++ DOM/activeElement.js
@@ -1,10 +1,2 @@
-
-//textContent get and set the element content it's mean it's override the existing content
-
-//createTextNode use create indepent text and add it to any element 
-const h1Element=document.getElementById('demo');
-const gettextcontenet=document.createTextNode(' is hubmle person')
-console.log(gettextcontenet)
-h1Element.appendChild(gettextcontenet);
-
-document.body.append(h1Element);
+let getColorForm=document.forms.namedItem('myColorForm');
+console.log(getColorForm)
