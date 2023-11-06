e95be15a844352460a7f1d8824fab8ae59cb6fcb
jobayed24 <jkhouse117@gmail.com>
2023-11-06 13:46:10 +0600

    get images by images method (Small Project)

--- DOM/activeElement.js
+++ DOM/activeElement.js
@@ -1,2 +1,12 @@
-let getHead=document.head.tagName;
-console.log(getHead);
+let getImg=document.images;
+const getLinks=[];
+for( let i=0; i<getImg.length; i++){
+    getLinks.push(getImg[i].src);
+}
+const getHttps=getLinks.filter(checkFilter);
+
+function checkFilter(value){
+   return value.startsWith('https');
+}
+console.dir(getHttps)
+
