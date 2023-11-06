97628fd8864abf6bcb40d6ada8811cfc693e3e85
jobayed24 <jkhouse117@gmail.com>
2023-11-06 10:09:49 +0600

    to get HTML name use firstElementChild

--- DOM/activeElement.js
+++ DOM/activeElement.js
@@ -1,13 +1,2 @@
-setInterval("getFocused()",1);
-
-function getFocused(){
-    let text;
-    if(document.hasFocus()){
-        text='you focused on document';
-    }else{
-        text='you didn\'t focused on doucment';
-    }
-    document.getElementById("demo").innerHTML=text
-}
-
-
+let getHead=document.firstElementChild.tagName;
+console.log(getHead);
