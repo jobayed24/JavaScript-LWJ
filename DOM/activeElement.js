54d0490a70971a990476bc8098cad6aa457b58f3
jobayed24 <jkhouse117@gmail.com>
2023-11-06 20:38:53 +0600

    get the all links by links method
    get all links by using links method it's read only

--- DOM/activeElement.js
+++ DOM/activeElement.js
@@ -1,12 +1,2 @@
-let getImg=document.images;
-const getLinks=[];
-for( let i=0; i<getImg.length; i++){
-    getLinks.push(getImg[i].src);
-}
-const getHttps=getLinks.filter(checkFilter);
-
-function checkFilter(value){
-   return value.includes('.com/jk');
-}
-console.dir(getHttps)
-
+let getLinks=document.links[0].href;
+console.log(getLinks)
