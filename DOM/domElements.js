b06bfdcfb9c837fe39b3b6d40a91d16302c0efba
jobayed24 <jkhouse117@gmail.com>
2023-11-07 09:38:08 +0600

    Bubbling and Capturing addEventListner have third paramenter if it's pass true then it will capture it means first parent will elemnet will show up then innner element And the other hand it bubbling do reverse method

--- DOM/domElements.js
+++ DOM/domElements.js
@@ -1,3 +1,18 @@
-let getLink=document.getElementById('clickme');
-let getAccessKey=getLink.accessKey;
-console.log(getAccessKey)
+document.getElementById('div').addEventListener('click',function(){
+    newCallBack('You click Div');
+},false)
+document.getElementById('p').addEventListener('click',function(){
+    newCallBack('you click p element');
+},false)
+
+document.getElementById('div2').addEventListener('click',function(){
+    newCallBack('you click div2 element')
+},true)
+
+document.getElementById('p2').addEventListener('click',function(){
+    newCallBack('click p2 element')
+},true)
+
+function newCallBack(text){
+    document.getElementById('demo').innerHTML+=text;
+}
