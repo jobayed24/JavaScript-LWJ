29975ff3557a80c02503e3c4f12f3012e84cf106
jobayed24 <jkhouse117@gmail.com>
2023-11-07 10:27:39 +0600

    Move element move element to one ul to another ul list using appendchild or append method here i was think that i should remove data form afeter appneding it to another element but it make no sense to remove data here . it's fully move existing element to new element

--- DOM/domElements.js
+++ DOM/domElements.js
@@ -1,18 +1,9 @@
-document.getElementById('div').addEventListener('click',function(){
-    newCallBack('You click Div');
-},false)
-document.getElementById('p').addEventListener('click',function(){
-    newCallBack('you click p element');
-},false)
-
-document.getElementById('div2').addEventListener('click',function(){
-    newCallBack('you click div2 element')
-},true)
-
-document.getElementById('p2').addEventListener('click',function(){
-    newCallBack('click p2 element')
-},true)
-
-function newCallBack(text){
-    document.getElementById('demo').innerHTML+=text;
+let getButton=document.getElementById('moveItem');
+getButton.addEventListener('click',moveItem);
+function moveItem(){
+    let getMyList=document.getElementById('myList');
+    let getMyList2=document.getElementById('myList2');
+    let getItem=getMyList.lastElementChild;
+    getMyList2.appendChild(getItem);
+    
 }
