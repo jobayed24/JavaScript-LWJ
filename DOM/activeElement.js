db24477df02ec7293778145674afcb8d0506981b
jobayed24 <jkhouse117@gmail.com>
2023-11-07 08:21:22 +0600

    Remvoe empty text node using Normaliz mehtod
    add text node useing create text node and get childNodes after creating

--- DOM/activeElement.js
+++ DOM/activeElement.js
@@ -1,2 +1,18 @@
-let getLinks=document.links[0].href;
-console.log(getLinks)
+
+function addTextNode(){
+    const createTextNode=document.createTextNode("create Text Node.    ");
+    const getBody=document.body;
+    getBody.appendChild(createTextNode);
+    let getCC=document.getElementById('cc');
+    let getChildNodeLength= getBody.childNodes.length;
+    console.log(document.body.childNodes)
+    console.log(getChildNodeLength)
+    getCC.innerHTML=(getChildNodeLength)
+}
+
+function nomalizeDocument(){
+    document.normalize();
+    const getBody=document.body;
+    let getCC=document.getElementById('cc');
+    getCC.innerHTML=getBody.childNodes.length;
+}
