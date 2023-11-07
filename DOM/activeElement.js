6529eae081ede10f3824c0a4baac4454662b2df7
jobayed24 <jkhouse117@gmail.com>
2023-11-07 08:25:19 +0600

    get Doc Title and set Doc Title get Document title and set also by using same title property in javascript

--- DOM/activeElement.js
+++ DOM/activeElement.js
@@ -1,18 +1,2 @@
-
-function addTextNode(){
-    const createTextNode=document.createTextNode("create Text Node.    ");
-    const getBody=document.body;
-    getBody.appendChild(createTextNode);
-    let getCC=document.getElementById('cc');
-    let getChildNodeLength= getBody.childNodes.length;
-    console.log(document.body.childNodes)
-    console.log(getChildNodeLength)
-    getCC.innerHTML=(getChildNodeLength)
-}
-
-function nomalizeDocument(){
-    document.normalize();
-    const getBody=document.body;
-    let getCC=document.getElementById('cc');
-    getCC.innerHTML=getBody.childNodes.length;
-}
+let getDocTitle=document.title="JavaScript";
+console.log(getDocTitle);
