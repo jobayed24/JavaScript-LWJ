160164d68081335c168185a2a38d2363b04ecc39
jobayed24 <jkhouse117@gmail.com>
2023-11-07 09:08:54 +0600

    all about dom element accesskey accesskey set and return the value to activate or focus

--- DOM/activeElement.js
+++
@@ -1,2 +0,0 @@
-let getDocTitle=document.title="JavaScript";
-console.log(getDocTitle);
---
+++ DOM/domElements.js
@@ -0,0 +1,3 @@
+let getLink=document.getElementById('clickme');
+let getAccessKey=getLink.accessKey;
+console.log(getAccessKey)
