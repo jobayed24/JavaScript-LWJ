a6e02e4aaef9daaf28952d1fcd9a364c80e7e73e
jobayed24 <jkhouse117@gmail.com>
2023-11-16 10:51:55 +0600

    children return the dom elements it's not return the text node or comment node

--- DOM/domElements.js
+++ DOM/domElements.js
@@ -1,11 +1,5 @@
-function myButton(){
-    let getInputFeild=document.getElementById('textRang');
-    let getInputValue=getInputFeild.value;
-    let text;
-    if(isNaN(getInputValue) || getInputValue <1 || getInputValue > 10){
-        text='Value is Invalid';
-    }else{
-        text='value ok';
-    }
-    document.getElementById('demo').innerHTML=text;
+function getBodyChild(){
+    let getChild=document.body.children;
+    console.log(getChild)
+    document.getElementById('demo').innerHTML=getChild;
 }
