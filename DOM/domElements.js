c7eaa3cbd3e44de7b5fda551ad25040a132c2009
jobayed24 <jkhouse117@gmail.com>
2023-11-16 11:20:11 +0600

    get html collection through children get text by text element

--- DOM/domElements.js
+++ DOM/domElements.js
@@ -1,7 +1,5 @@
 function getBodyChild(){
-    let getChildren=document.getElementById('myDIV').children;
-    console.log(getChildren)
-    if(getChildren[1]){
-        getChildren[1].style='background-color:red'
-    }
+    let getChildren=document.getElementById('mySelect').children;
+    document.getElementById('demo').innerHTML=getChildren[1].text
+    console.log(getChildren[1])
 }
